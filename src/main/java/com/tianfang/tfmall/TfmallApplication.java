3358fe67b9fafe397761d937153f768a8c7c6e72
liuhao <liuhao@bd-ego.com>
2020-09-17 13:59:21 +0800

    搭建springcloud

---
+++ src/main/java/com/tianfang/tfmall/TfmallApplication.java
@@ -0,0 +1,13 @@
+package com.tianfang.tfmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TfmallApplication.class, args);
+    }
+
+}
